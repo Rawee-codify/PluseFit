fa3e2c0c079083d09c24b6b0c4641730a8f19d12
Ravindu_Dilshan <153814102+Rawee-codify@users.noreply.github.com>
2024-12-16 21:30:37 +0530

    creating authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plusefit/core/utils/Home/wrapper.dart';
 import 'package:plusefit/core/utils/get_Started/get_started.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'plusefit',
-      home: GetStarted(),
+      home: Wrapper(),
 
     );
   }
