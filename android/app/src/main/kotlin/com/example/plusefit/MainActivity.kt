0ffb1916a62b0bffcdd87cb8684c9e19c99d7927
Ravindu_Dilshan <153814102+Rawee-codify@users.noreply.github.com>
2024-12-16 16:20:30 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/plusefit/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.plusefit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/core/utils/get_Started/get_started.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class GetStarted extends StatefulWidget {
+  const GetStarted({super.key});
+
+  @override
+  State<GetStarted> createState() => _GetStartedState();
+}
+
+class _GetStartedState extends State<GetStarted> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          // Background image
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/background_image.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
