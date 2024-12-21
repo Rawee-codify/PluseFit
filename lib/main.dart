99d51fb6c3a004a3b4de132acfa55e5fda559c04
Ravindu_Dilshan <153814102+Rawee-codify@users.noreply.github.com>
2024-12-21 11:46:44 +0530

    architecture change to clean architecture.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:plusefit/core/utils/Home/wrapper.dart';
-import 'package:plusefit/core/utils/get_Started/get_started.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'plusefit',
-      home: Wrapper(),
+
 
     );
   }
