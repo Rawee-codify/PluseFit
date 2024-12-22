2ddf560d2276e4f839a25f7446e29b55667e5b47
Ravindu_Dilshan <153814102+Rawee-codify@users.noreply.github.com>
2024-12-22 14:57:41 +0530

    get started page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plusefit/screen/get_started.dart';
 
 
 void main() {
@@ -10,9 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'plusefit',
-
+      home:GetStartedPage(),
 
     );
   }
